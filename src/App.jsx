c192a12f07e4ea0dfe0012cbafa966b92af59d85
sapardi <sapardigo@gmail.com>
2025-08-09 21:16:07 +0700

    menambahkan file Login.modul di components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Login from "./pages/Login";
+
+const style = {
+  color: "white",
+  backgroundColor: "lightcoral",
+  padding: "10px",
+  borderRadius: "5px",
+};
+
+const paragraf = {
+  color: "white",
+  fontSize: "20px",
+  backgroundColor: "green",
+  padding: "4px",
+  borderRadius: "5px",
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Login />
+      <h1 style={style}>Halo, Nama saya Sapardi</h1>
+      <p style={paragraf}>Ini contoh deskripsi</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
