8e8592d739cab28348bcaad616f83d75b2104725
sapardi <sapardigo@gmail.com>
2025-08-09 22:28:24 +0700

    ternyata className bukan style

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,11 @@
+import styles from "../components/Login.module.css";
+
 export default function Login() {
   return (
-    <div>
-      <p>Ini halaman Login</p>
-    </div>
+    <>
+      <div>
+        <p className={styles.pada}>Ini halaman Login</p>
+      </div>
+    </>
   );
 }
